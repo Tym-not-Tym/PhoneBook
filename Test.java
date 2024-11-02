bc9fc50b9bb462784837ff45d7e3d248d71f4970
Tym Grebenyuk <tima.grebenyuk@gmail.com>
2024-11-01 18:22:50 -0700

    Added coments to the Test class

--- Test.java
+++ Test.java
@@ -1,3 +1,12 @@
+/*Tymofiy Grebenyk
+ * Assignment 2: Phone Book
+ * 
+ * A simple Java program that implements a phone book application. 
+ * This program allows users to add, search, and delete contacts 
+ * within a phone book. Each contact includes basic information 
+ * such as name, phone number, address.
+ */
+
 public class Test {
     public static void main(String[] args) {
         Manager phoneBook = new Manager();
